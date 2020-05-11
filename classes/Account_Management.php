225aaaeb9db7df83ff4ce6498da042d194299b09
Husseinali4 <32770053+Husseinali4@users.noreply.github.com>
2020-05-11 21:11:48 +0200

    Add files via upload

---
+++ classes/Account_Management.php
@@ -0,0 +1,12 @@
+<?php
+
+interface Account_Management{
+
+	/* Start class's functions */	
+
+	public function editAccount($person,$oldname);
+
+	public function DeleteAccount($person);
+	
+	/* End class's functions*/	
+}
